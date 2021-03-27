7efa1d31a432605a089166336064f250a9b7d02a
aleksinap <aleksinap@yandex.ru>
2021-03-27 22:48:17 +0300

    Initial commit

---
+++ src/main/kotlin/ru/netology/service/WallService.kt
@@ -0,0 +1,37 @@
+package ru.netology.service
+
+import ru.netology.data.Post
+import kotlin.random.Random
+
+class  WallService() {
+    private var posts = emptyArray<Post>()
+
+    fun add(post: Post): Post {
+        var unicId: Int = post.id
+        fun isUnic() : Boolean{
+            for(post in posts){
+                if(post.id == unicId) return false
+            }
+            return true
+        }
+        while (!isUnic()) {
+            unicId = Random.nextInt(1, 5)
+        }
+        posts += post.copy(id = unicId)
+        return posts.last()
+    }
+
+    fun update(post: Post) : Boolean {
+        for(i in posts.indices){
+            if(posts[i].id == post.id){
+                posts[i] = post.copy(id = posts[i].id, ownerId = posts[i].ownerId)
+                return true
+            }
+        }
+        return false
+    }
+
+    fun show(){
+        for(post in posts) println("${post.id}, ${post.createdBy}, ${post.text}")
+    }
+}
